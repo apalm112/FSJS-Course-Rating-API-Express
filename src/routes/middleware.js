fe1d54af57e6885703b5c96ca7c180406ba3d28b
Adam <apalm112@gmail.com>
2018-10-12 00:00:52 -0400

    Commented out the original middleware function, broke it into three smaller functions for unit testing.  Also have a Promise based version of the function commented out, it works but probably won't use it.

--- src/routes/middleware.js
+++ src/routes/middleware.js
@@ -2,9 +2,9 @@
 
 /* Custom Middleware to check for Header Authentiation **************/
 const auth = require('basic-auth');
-var User = require('../database/models').User;
+const User = require('../database/models').User;
 
-function credentials(req, res, next) {
+/*exports.credentials = function(req, res, next) {
 	var getAuthorization = auth(req);
 	// Get the user's email & password from the headers.
 	if (!getAuthorization.name || !getAuthorization.pass) {
@@ -23,6 +23,68 @@ function credentials(req, res, next) {
 			}
 		});
 	}
-}
+// module.exports.credentials = credentials;
+};*/
 
-module.exports.credentials = credentials;
+
+exports.credentials = function(req, res, next) {
+	var getAuthorization = auth(req);
+	// Get the user's email & password from the headers.
+	if (getAuthorization.name && getAuthorization.pass) {
+		return next();
+	} else {
+		var error = new Error('Email and password are required from custom middleware.');
+		error.status = 401;
+		return next(error);
+	}
+};
+
+exports.callAuthen = function(req, res, next) {
+	var getAuthorization = auth(req);
+// Calls the User Schema static method to authenticate the user.
+	User.authenticate(getAuthorization, function(error, user) {
+		if ( !user ) {
+			return next(error);
+		} else {
+			res.locals.user = user.id;
+			req.body.user = user;
+			next();
+		}
+	});
+};
+
+exports.result = function (req, res) {
+	res.json(req.body.user);
+};
+
+
+/*exports.credentials = function(req, res) {
+	return new Promise(function(resolve, reject) {
+		var getAuthorization = auth(req);
+		// Get the user's email & password from the headers.
+		if (getAuthorization.name || getAuthorization.pass) {
+			// Calls the User Schema static method to authenticate the user.
+			User.authenticate(getAuthorization, function(error, user) {
+				if (user) {
+					res.locals.user = user.id;
+					req.body.user = user;
+					resolve(req.body.user);
+				} else {
+					var error = new Error('Email and password are required from custom middleware.');
+					error.status = 401;
+					reject(error);
+				}
+			});
+		}
+	});
+};
+// TODO: Write Unit Test for GET /api/user route:
+router.get('/users', (req, res) => {
+	middle.credentials(req, res)
+		.then(response => res.json(response))
+		.catch(error => res.json(error));
+});
+
+
+
+*/
